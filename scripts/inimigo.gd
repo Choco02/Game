7f6f674fcde13a39f3db5059c8adfbd422fd6674
Choco <wesleyassis455@gmail.com>
2019-01-18 02:29:27 -0200

    adicionado leve movimento ao inimigo, esperando algoritmo

--- scripts/inimigo.gd
+++ scripts/inimigo.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 const GRAVITY = 100
 var motion = Vector2()
+var andar = 100
 
 
 func _ready():
@@ -16,6 +17,8 @@ func _physics_process(delta):
 func normal(delta):
 	motion.y += GRAVITY
 	#print(motion.y)
+	motion.x = andar
+	var sprite = get_node('Sprite')
 	motion = move_and_slide(motion)
 	
 #inimigo esperando movimentacao
